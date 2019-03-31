cdd707b7789ea8282e9f3dad9f5d44428d78448e
Zeavel <34531943+Zeavel@users.noreply.github.com>
2019-03-31 17:40:02 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -566,9 +566,9 @@ member.send(msg.content)
   }
     else
     {
-      client2.guilds.get("351491707554103296").guilds.get("351491707554103296").guilds.get("351491707554103296").channels.get("561257588075790336").fetchMessage("561257965995294731")
+      client2.guilds.get("351491707554103296").channels.get("561257588075790336").fetchMessage("561257965995294731")
  .then(ser=>{
-        client2.guilds.get("351491707554103296").guilds.get("351491707554103296").channels.get("561257588075790336").fetchMessage("561257973482258472")
+        client2.guilds.get("351491707554103296").channels.get("561257588075790336").fetchMessage("561257973482258472")
  .then(ch1=>{
        client2.guilds.get(ser).channels.get(ch1).send("<@"+member.id+">"+msg.content)
         })
